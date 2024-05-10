1cb9f5ee57c467e72c01eb17770fc89b815dc50e
imhayatunnabi <nabilahmednafiahmed753@gmail.com>
2024-05-10 12:07:23 +0600

    Add task Button component and react icons has been added

---
+++ app/components/AddTask.tsx
@@ -0,0 +1,9 @@
+const AddTask = () => {
+  return (
+    <div>
+        <button className="btn btn-primary w-full">Add New Task</button>
+    </div>
+  )
+}
+
+export default AddTask
--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,12 @@
 
+import AddTask from './components/AddTask';
 export default function Home() {
   return (
-    <main className="max-w-4xl">
-      
+    <main className="max-w-4xl mx-auto mt-4">
+      <div className="text-center my-5 flex flex-col gap-4">
+        <h1 className="text-2xl font-bold">Next Todo List App</h1>
+        <AddTask></AddTask>
+      </div>
     </main>
   );
 }
